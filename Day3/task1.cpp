5afb4754bd70c976941cefc2c7908b985c775ddc
mani1709 <58658612+mani1709@users.noreply.github.com>
2021-12-03 17:15:53 +0100

    day 3 completed

---
+++ Day3/task1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h> 
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream inFile;
+    inFile.open("C:\\Users\\User\\OneDrive\\Desktop\\AdventOfCode\\Day3\\input.txt");
+
+    vector<string> textLines;
+
+    string nextString;
+    while (inFile >> nextString) {
+        textLines.push_back(nextString);
+    }
+
+    int stringLength = textLines[0].size();
+
+    string gammaRateString;
+    string epsilonRateString;
+
+    for(int i = 0; i < stringLength; i++) {
+        int amountBitZero = 0;
+        int amountBitOne = 0;
+        for(int j = 0; j < textLines.size(); j++) {
+            if(textLines[j][i] == '0') {
+                amountBitZero++;
+            } else {
+                amountBitOne++;
+            }
+        }
+
+        cout << amountBitZero << " " << amountBitOne << endl;
+
+        if(amountBitZero < amountBitOne) {
+            gammaRateString += "0";
+            epsilonRateString += "1";
+        } else {
+            gammaRateString += "1";
+            epsilonRateString += "0";
+        }
+    }
+
+    int gammaRate = 0;
+    int epsilonRate = 0;
+
+    for(int i = 0; i < stringLength; i++) {
+        gammaRate += ((int) gammaRateString[i] - 48) * pow(2, stringLength-1-i);
+        epsilonRate += ((int) epsilonRateString[i] - 48) * pow(2, stringLength-1-i);
+    }
+
+    cout << "Power consumption: " << gammaRate * epsilonRate;
+
+    return 0;
+}
---
+++ Day3/task2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h> 
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream inFile;
+    inFile.open("C:\\Users\\User\\OneDrive\\Desktop\\AdventOfCode\\Day3\\input.txt");
+
+    vector<string> textLines;
+
+    string nextString;
+    while (inFile >> nextString) {
+        textLines.push_back(nextString);
+    }
+
+    int stringLength = textLines[0].size();
+
+    string oxygenGeneratorRatingString;
+    string coTwoScrubberRatingString;
+
+    vector<string> filtered = textLines;
+    for(int i = 0; i < stringLength; i++) {
+        int amountZeros = 0;
+        int amountOnes = 0;
+
+        for(int j = 0; j < filtered.size(); j++) {
+            filtered.at(j)[i] == '0' ? amountZeros++ : amountOnes++;
+        }
+        
+        if(amountZeros > amountOnes) {
+            for(int j = 0; j < filtered.size(); j++) {
+                if(filtered.at(j)[i] == '1') {
+                    filtered.erase(filtered.begin()+j);
+                    j--;
+                }
+            }
+        } else {
+            for(int j = 0; j < filtered.size(); j++) {
+                if(filtered.at(j)[i] == '0') {
+                    filtered.erase(filtered.begin()+j);
+                    j--;
+                }
+            }
+        }
+        
+        if(filtered.size() == 1) {
+            oxygenGeneratorRatingString = filtered.at(0);
+            break;
+        }
+    }
+
+    filtered = textLines;
+    for(int i = 0; i < stringLength; i++) {
+        int amountZeros = 0;
+        int amountOnes = 0;
+
+        for(int j = 0; j < filtered.size(); j++) {
+            filtered.at(j)[i] == '0' ? amountZeros++ : amountOnes++;
+        }
+        
+        if(amountZeros > amountOnes) {
+            for(int j = 0; j < filtered.size(); j++) {
+                if(filtered.at(j)[i] == '0') {
+                    filtered.erase(filtered.begin()+j);
+                    j--;
+                }
+            }
+        } else {
+            for(int j = 0; j < filtered.size(); j++) {
+                if(filtered.at(j)[i] == '1') {
+                    filtered.erase(filtered.begin()+j);
+                    j--;
+                }
+            }
+        }
+
+        if(filtered.size() == 1) {
+            coTwoScrubberRatingString = filtered.at(0);
+            break;
+        }
+    }
+
+    cout << oxygenGeneratorRatingString << " " << coTwoScrubberRatingString << endl;
+
+    int oxygenGeneratorRating = 0;
+    int coTwoScrubberRating = 0;
+
+    for(int i = 0; i < stringLength; i++) {
+        oxygenGeneratorRating += ((int) oxygenGeneratorRatingString[i] - 48) * pow(2, stringLength-1-i);
+        coTwoScrubberRating += ((int) coTwoScrubberRatingString[i] - 48) * pow(2, stringLength-1-i);
+    }
+
+    cout << oxygenGeneratorRating << " " << coTwoScrubberRating << endl;
+
+    cout << "Life support rating: " << oxygenGeneratorRating * coTwoScrubberRating;
+
+    return 0;
+}
